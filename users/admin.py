4ffded90375d00e578be02fb68e03bb63d9efb03
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2025-12-04 18:18:51 +0200

    Created second user and completed serializers,views, admin for users app.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ["id", "username", "email", "is_barber", "is_client", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (
+        ("Roluri", {"fields": ("is_barber", "is_client", "phone")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Roluri", {"fields": ("is_barber", "is_client", "phone")}),
+    )
---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'phone', 'is_barber', 'is_client']
+        read_only_fields = ['id', 'is_barber', 'is_client']
