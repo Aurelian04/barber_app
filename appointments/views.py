b3a50ca57af29c0f40489d43c50ed0cb1ed572a8
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2026-02-25 13:54:23 +0200

    Created ClientAppointmentView

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import AppointmentSerializer
+from .models import Appointment
+
+
+class ClientAppointmentView(ModelViewSet):
+    serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Appointment.objects.filter(client=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(client=self.request.user)
