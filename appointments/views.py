55d2d4febfd1c7f54ca50de2cc36bb44d1bfb4a5
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2026-02-27 09:36:33 +0200

    Finish class BarberAppointmentListView

--- appointments/views.py
+++ appointments/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from .serializers import AppointmentSerializer
 from .models import Appointment
@@ -16,3 +16,23 @@ class ClientAppointmentView(ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(client=self.request.user)
+        
+        
+class BarberAppointmentListView(ReadOnlyModelViewSet):
+    """Barber see only his clients appointments."""
+    serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_barber:
+            return Appointment.objects.none()
+        return Appointment.objects.filter(barber=user)
+
+    
+    
+    
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     return Appointment.objects.filter(barber=user)
