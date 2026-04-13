85aa715d82658aaf947d310669c0ac8f2e5ab915
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2026-04-13 16:27:51 +0300

    Added api_auth in urls/core and started first auto test in availability

--- availability/tests.py
+++ availability/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from datetime import timedelta
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import BarberWeeklySchedule, LunchBreak, BarberScheduleException
+
+class AvailabilityApiTests(APITestCase):
+    def setUp(self):
+        self.user = get_user_model()
+        
+        self.barber_user = self.User.objects.create_user(
+            username="barber",
+            email="barber@example.com",
+            password="testpass123",
+            is_barber=True,
+        )
+        
+    def test_barber_can_create_weekly_schedule(self):
+        self.client.force_authenticate(barber=self.barber_user)
+        
+        payload = {
+            "weekday": "Monday",
+            "start_time": "9:00",
+            "end_time": "17:00",
+            "is_active": "True",
+        }
+        
+        url = (r"/api/barber/weekly-schedules/")
+        
+        response = self.client.post(url, payload, format="json")
+        
+        
--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ def home(request):
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
+    path("api-auth/", include("rest_framework.urls")),
 
     # DRF Users API
     path("api/user/", include("users.urls")),
