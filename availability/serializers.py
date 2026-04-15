bcfb12fb93d0c292deceb5892ba6062f9c2157fb
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2026-04-15 16:33:31 +0300

    Modifyde serializer, now test works as expected

--- availability/serializers.py
+++ availability/serializers.py
@@ -1,7 +1,14 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework import serializers
 
 from .models import BarberWeeklySchedule, LunchBreak, BarberScheduleException 
 
+
+
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework import serializers
+
+from .models import BarberWeeklySchedule
 
 
 class BarberWeeklyScheduleSerializer(serializers.ModelSerializer):
@@ -16,21 +23,30 @@ class BarberWeeklyScheduleSerializer(serializers.ModelSerializer):
             "is_active",
         ]
         read_only_fields = ["id", "barber"]
-        
+
     def validate(self, attrs):
         request = self.context.get("request")
-        
+
         if request is None or not request.user.is_authenticated:
             raise serializers.ValidationError("Authentication is required.")
-        
+
         if not request.user.is_barber:
             raise serializers.ValidationError("Only barbers can manage weekly schedules.")
-        
+
         return attrs
-    
+
     def create(self, validated_data):
         validated_data["barber"] = self.context["request"].user
-        return super().create(validated_data)
+        try:
+            return super().create(validated_data)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(e.message_dict)
+
+    def update(self, instance, validated_data):
+        try:
+            return super().update(instance, validated_data)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(e.message_dict)
     
     
 class LunchBreakSerializer(serializers.ModelSerializer):
