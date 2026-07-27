ce88bf3efa3ff3cc48a2e27cb4711730f6cce45a
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2026-07-27 21:00:34 +0300

    Added cors modifications in setting middleware first line, cors_allowed_origins and corsheader in apps. Frontent progres work very good soon prepare the ters commit with first pages complete. axios works good, created two users from frontend app in browser

--- core/settings.py
+++ core/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     "services",
     "appointments",
     "availability",
+    "corsheaders",
 ]
 
 AUTH_USER_MODEL = "users.User"
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,6 +57,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = "core.urls"
