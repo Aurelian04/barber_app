a4836afe055271a82270b2b4772768d0ca1d74e9
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2026-07-28 17:19:00 +0300

    Completed JWT in drf and works good, recived both tokens now work in frontend for jwt. added rest_framework in settings and added specific urls in urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 def home(request):
     return HttpResponse("Welcome to the Home Page")
@@ -9,6 +13,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
     path("api-auth/", include("rest_framework.urls")),
+    
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     # DRF Users API
     path("api/user/", include("users.urls")),
